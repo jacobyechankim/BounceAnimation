b16fee36d17407d22df5edbec16eb761e641de59
jacobyechankim <jacobyechankim@gmail.com>
2025-04-21 20:53:18 +0900

    Initial Commit

---
+++ BounceAnimation/BounceAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BounceAnimationApp.swift
+//  BounceAnimation
+//
+//  Created by 0000 on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BounceAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BounceAnimationTests/BounceAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  BounceAnimationTests.swift
+//  BounceAnimationTests
+//
+//  Created by 0000 on 4/21/25.
+//
+
+import Testing
+@testable import BounceAnimation
+
+struct BounceAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
